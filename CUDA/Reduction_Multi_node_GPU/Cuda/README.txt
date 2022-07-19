2a24d8ad5a27985a75baaa2312e15d4b02f8e272
40623128 <40623128@gm.nfu.edu.tw>
2022-07-19 14:12:10 +0800

    更改資料交換為 MPI_Scatter 與 MPI_Gather

--- CUDA/Reduction_Multi_node_GPU/Cuda/README.txt
+++ CUDA/Reduction_Multi_node_GPU/Cuda/README.txt
@@ -1,4 +1,5 @@
 module load cuda/11.3
+module load openmpi4/4.1.1
 nvcc \
 -I/work/HPC_SYS/twnia2/openmpi/openmpi-4.1.1-ucx-1.10.1-cuda-11.3/include \
 -L/work/HPC_SYS/twnia2/openmpi/openmpi-4.1.1-ucx-1.10.1-cuda-11.3/lib \
--- CUDA/Reduction_Multi_node_GPU/Cuda/multi_node.cu
+++ CUDA/Reduction_Multi_node_GPU/Cuda/multi_node.cu
@@ -5,11 +5,8 @@
 
 using namespace std;
 
-//const int num_gpus = 2;
-//經測試於RTX3070最佳值為128，接著為256。
 const int threadsPerBlock = 128;
-//相加之元素個數(2^30-3)
-const int Total_N               = (1 <<10 );
+const int Total_N         = (1<<10);
 const int iters           = 1;
 //const int kernal_number = 7;
 //kernel1
@@ -41,7 +38,8 @@ __global__ void kernel1(double* arr, double* out, int N){
 int main(int argc, char *argv[]) {
 	
 	/************************
-	*MPI初始化           	*
+	*MPI初始化              *
+	*得到當前的process 數目 *
 	*得到當前的rank(秩)  	*
 	*得到目前處理器的名稱	*
 	************************/
@@ -51,70 +49,87 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_rank (MPI_COMM_WORLD, &world_rank);
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int name_len;
-	MPI_Get_processor_name(processor_name,&name_len);
+	MPI_Get_processor_name(processor_name, &name_len);
 
+	/*************************
+	*num_gpus   單節點GPU數量*
+	*total_gpus 共使用GPU數量*
+	*device     各GPU編號    *
+	*cudaError_t error_code  *
+	*************************/
 	int num_gpus;
+	int total_gpus;
 	int device;
 	cudaError_t err;
 	cudaGetDeviceCount(&num_gpus);
+	//num_gpus = 2;
+	total_gpus = num_gpus*world_size;
 	printf("node_%d_num_gpus = %d\n", world_rank,num_gpus);
-	//err = cudaGetDevice(&device);
-	//printf("cudaGetDevice = %d\n",err);
-
-	const int N = Total_N/num_gpus;
-	const int blocksPerGrid   = (N + threadsPerBlock - 1)/threadsPerBlock;
-	float total_time[num_gpus];
-	double* a_host[num_gpus], *r_host[num_gpus];
-	double* a_device[num_gpus], *r_device[num_gpus];
+	printf("total_gpus = %d\n", total_gpus);
+	
+	const int Node_N = Total_N/world_size;
+	const int GPU_N = Node_N/num_gpus;
+	const int blocksPerGrid   = (GPU_N + threadsPerBlock - 1)/threadsPerBlock;
+	float total_time[total_gpus];
+	double* a_host[total_gpus], *r_host[total_gpus];
+	double* a_device[total_gpus], *r_device[total_gpus];
+	double* All_host;
+	
+	//題目生成
+	if(world_rank == 0){
+		cudaMallocHost(&All_host, Total_N * sizeof(double));
+		for(int i=0;i<Total_N;i++){
+			All_host[i] = 1.0;
+		}
+	}
+	//node同步
+	//MPI_Barrier(MPI_COMM_WORLD);
+	double *node_host = (double *)malloc(sizeof(double) * Node_N);
+	//題目分配各節點
+	MPI_Scatter(All_host, Node_N, MPI_DOUBLE,
+				node_host, Node_N, MPI_DOUBLE,
+				0, MPI_COMM_WORLD);
+	printf("MPI_Scatter Finshed\n");
 	for(int i = 0; i < num_gpus; i++){
 		//主機內存分配
-		cudaMallocHost(&a_host[i], N * sizeof(double));
+		cudaMallocHost(&a_host[i], GPU_N * sizeof(double));
 		cudaMallocHost(&r_host[i], blocksPerGrid * sizeof(double));
-		//顯卡內存分配
 	}
-	//cout << "Memory Allocation Completed" << endl;
-
-
-	//題目生成
-	//cout << "Generating list" << endl;
+	//printf("host Memory location Finshed\n");
 	for(int i = 0; i < num_gpus; i++){
-		for(int j=0;j<N;j++){
-			a_host[i][j] = 1.0+i*N+j;
-		}
-		//cout << "a_host "<< i <<" Generating Completed" << endl;
-		for(int j=0;j<blocksPerGrid;j++){
-			r_host[i][j] = 0.0;
-		}
-		//cout << "r_host "<< i <<" Generating Completed" << endl;
+		for(int j = 0; j < GPU_N; j++){
+			a_host[i][j] = node_host[i*GPU_N+j];
+			//printf("a_host[%d][%d] = %f\n", i, j , a_host[i][j]);
+			}
 	}
-
+	//printf("a_host location Finshed\n");
+	//printf("Memory Allocation Completed\n");
 
 	//定義開始和停止事件(Event)
 	cudaStream_t stream[num_gpus];
 	cudaEvent_t start_events[num_gpus];
 	cudaEvent_t stop_events[num_gpus];
 	float elapsedTime[num_gpus];
-
+	
 	//定義顯卡流
 	for(int i = 0; i < num_gpus; i++){
 		err = cudaSetDevice(i);
-		//printf("GPU %d Set Device = %d\n",i,err);
 		err = cudaGetDevice(&device);
-		//printf("GPU %d Get Device = %d\n",i,err);
-		//定義顯卡流
 		cudaStreamCreate(&stream[i]);
+		//printf("GPU %d Set Device = %d\n",i,err);
+		//printf("GPU %d Get Device = %d\n",i,err);
 		//printf("GPU %d Stream Define Completed\n",i);
-
-		cudaMalloc(&a_device[i], N * sizeof(double));
+		
+		cudaMalloc(&a_device[i], GPU_N * sizeof(double));
 		cudaMalloc(&r_device[i], blocksPerGrid * sizeof(double));
 
 
 
-		//記憶體設定(異步)
-		cudaMemcpyAsync(a_device[i], a_host[i], N * sizeof(double),
-						cudaMemcpyHostToDevice,stream[i]);
+		//記憶體複製(異步)
+		cudaMemcpyAsync(a_device[i], a_host[world_rank*num_gpus+i], GPU_N * sizeof(double),
+						cudaMemcpyHostToDevice, stream[i]);
 		cudaMemcpyAsync(r_device[i], r_host[i], blocksPerGrid * sizeof(double),
-						cudaMemcpyHostToDevice,stream[i]);
+						cudaMemcpyHostToDevice, stream[i]);
 		//printf("Mem ERROR GPU %d = %s\n",i,cudaGetErrorString(cudaGetLastError()));
 		//printf("Memory asynchronous Completed\n");
 
@@ -126,20 +141,21 @@ int main(int argc, char *argv[]) {
 		//printf("Start Calculation\n");
 		cudaEventRecord(start_events[i],stream[i]);
 		//運用Kernel1進行運算
-		kernel1<<<blocksPerGrid, threadsPerBlock, 0,stream[i]>>>(a_device[i], r_device[i], N);
+		kernel1<<<blocksPerGrid, threadsPerBlock, 0,stream[i]>>>(a_device[i], r_device[i], GPU_N);
 		//紀錄停止事件(Event)
 		cudaEventRecord(stop_events[i],stream[i]);
 		cudaDeviceSynchronize();
 		cudaEventSynchronize(stop_events[i]);
-		printf("node_%d_GPU_%d ERROR = %s\n", world_rank, i, cudaGetErrorString(cudaGetLastError()));
+		//printf("node_%d_GPU_%d ERROR = %s\n", world_rank, i, cudaGetErrorString(cudaGetLastError()));
 		//printf("GPU %d Calculation Completed\n",i);
 
 		//計算開始事件至暫停事件所經時間
 		//printf("GPU %d Calculation time\n",i);
 		cudaEventElapsedTime(&elapsedTime[i], start_events[i], stop_events[i]);
 		total_time[i] = total_time[i] + (elapsedTime[i] / iters);
-		//cout << "total_time "<< i << "= " << total_time[i] << endl;
-		//cout << "elapsedTime "<< i << "= " << elapsedTime[i] << endl;
+		//printf("total_time %d = %f Calculation time\n", i, total_time[i]);
+		//printf("elapsedTime %d = %f Calculation time\n", i, elapsedTime[i]);
+		
 		if (i ==0){
 			total_time[i] = total_time[i];
 		}
@@ -147,14 +163,13 @@ int main(int argc, char *argv[]) {
 			total_time[i] = total_time[i-1] + total_time[i];
 		}
 
-		//cout << "Event Destroy" << endl;
+		//printf("Event Destroy\n");
 		cudaEventDestroy(start_events[i]);
 		cudaEventDestroy(stop_events[i]);
 
 		//資料由顯卡記憶體傳輸至主機記憶體
-		//cout << "Share Memory form Device to Host" << endl;
-		cudaMemcpy(r_host[i], r_device[i], blocksPerGrid * sizeof(double),
-								  cudaMemcpyDeviceToHost);
+		//printf("Share Memory form Device to Host\n");
+		cudaMemcpy(r_host[i], r_device[i], blocksPerGrid * sizeof(double), cudaMemcpyDeviceToHost);
 		//printf("node_%d_GPU_%d Elapse time for The Kernal 1 : %f ms\n", world_rank, i, total_time[i]);
 		total_time[i] = 0.0 ;
 		elapsedTime[i] = 0.0 ;
@@ -169,8 +184,21 @@ int main(int argc, char *argv[]) {
 			//printf("r_host[%d][%d] = %f\n", i, j, r_host[i][j]);
 			//printf("Ans [%d][%d] = %f\n", i, j, r_host[0][0]);
 		}
-		//printf("r_host[0][0] = %f\n",r_host[0][0]);
-
+		//printf("node_%d_GPU_%d = %f\n", world_rank, i, r_host[0][0]);
+		//printf("r_host[0][0] = %f\n", r_host[0][0]);
+	}
+	
+	double *All_Ans = (double *)malloc(sizeof(double) * world_size);;
+	MPI_Gather(&r_host[0][0], 1, MPI_DOUBLE, All_Ans, 1, MPI_DOUBLE, 0,
+           MPI_COMM_WORLD);
+	if (world_rank == 0){
+		double final_Ans;
+		for(int i = 0; i < world_size; i++){
+		final_Ans += All_Ans[i];
+		}
+		printf("final_Ans = %f\n", final_Ans);
+	}
+	for(int i = 0; i < num_gpus; i++){
 		//記憶體釋放
 		cudaFree(r_device[i]);
 		cudaFreeHost(r_host[i]);
